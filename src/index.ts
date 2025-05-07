e56be25db32bf5786a3b396cff7553cff52ce754
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-05-08 02:06:50 +0600

    problem slve 4

--- src/index.ts
+++ src/index.ts
@@ -33,3 +33,31 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 
 // console.log(concatenateArrays(["a", "b"], ["c"]));        
 // console.log(concatenateArrays([1, 2], [3, 4], [5]));       
+
+
+
+
+
+//problem 4
+class Vehicle {
+  constructor(private make: string, private year: number) {}
+
+  getInfo(): string {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make: string, year: number, private model: string) {
+    super(make, year);
+  }
+
+  getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+console.log(myCar.getInfo());  
+console.log(myCar.getModel());  
