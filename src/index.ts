756f673941b1f70692a32b90e71be99a800ac25c
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-05-08 01:57:47 +0600

    problem 2 slove

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,17 @@ function formatString(input: string, toUpper: boolean = true): string {
 // console.log(formatString("Hello", false));   
 
 
+
+
+//Problem 2
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+  return items.filter(item => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+// console.log(filterByRating(books));
