17f22533202bc26abb24118d9097f02595820be4
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-05-08 01:52:49 +0600

    problem 1 slove

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+//Problem 1
+function formatString(input: string, toUpper: boolean = true): string {
+  return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
+
+// console.log(formatString("Hello"));          
+// console.log(formatString("Hello", true));    
+// console.log(formatString("Hello", false));   
+
+
