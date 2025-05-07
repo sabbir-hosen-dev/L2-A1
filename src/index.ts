396431c337612d15de7d63aefdbe86432ee74f1a
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-05-08 02:45:49 +0600

    comment remove

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
-//Problem 1
+
 function formatString(input: string, toUpper: boolean = true): string {
   return toUpper ? input.toUpperCase() : input.toLowerCase();
 }
 
-// console.log(formatString("Hello"));          
-// console.log(formatString("Hello", true));    
-// console.log(formatString("Hello", false));   
 
-
-
-
-//Problem 2
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
   return items.filter(item => item.rating >= 4);
 }
@@ -21,24 +14,12 @@ const books = [
   { title: "Book C", rating: 5.0 }
 ];
 
-// console.log(filterByRating(books));
-
-
-
-
-//Problem 3 
+ 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arrays.flat();
 }
 
-// console.log(concatenateArrays(["a", "b"], ["c"]));        
-// console.log(concatenateArrays([1, 2], [3, 4], [5]));       
 
-
-
-
-
-//problem 4
 class Vehicle {
   constructor(private make: string, private year: number) {}
 
@@ -58,23 +39,12 @@ class Car extends Vehicle {
 }
 
 
-// const myCar = new Car("Toyota", 2020, "Corolla");
-// console.log(myCar.getInfo());  
-// console.log(myCar.getModel());  
 
-
-//Problem 5
 function processValue(value: string | number): number {
   return typeof value === "string" ? value.length : value * 2;
 }
 
-// console.log(processValue("hello")); 
-// console.log(processValue(10));      
 
-
-
-
-//problem 6
 interface Product {
   name: string;
   price: number;
@@ -91,10 +61,7 @@ const products = [
   { name: "Notebook", price: 25 },
   { name: "Bag", price: 50 }
 ];
-// console.log(getMostExpensiveProduct(products));  
 
-
-//problem 7 
 
 enum Day {
   Monday,
@@ -111,11 +78,10 @@ function getDayType(day: Day): string {
 }
 
 
-// console.log(getDayType(Day.Monday));   
-// console.log(getDayType(Day.Sunday));   
+  
 
 
-//problem 8 
+
 
 async function squareAsync(n: number): Promise<number> {
   return new Promise((resolve, reject) => {
@@ -126,6 +92,5 @@ async function squareAsync(n: number): Promise<number> {
 }
 
 
-// squareAsync(4).then(console.log);     
-// squareAsync(-3).catch(console.error);    
+  
 
